5acdd8b753851204e1178a06ea59b54094d47edc
Dongsu Park <dongsu.park@profitbricks.com>
2014-12-04 17:51:59 +0100

    Revert "plugging"
    This reverts commit d666288f852c0f8214529d06cde5e6dd21c3a5b0.

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -953,7 +953,9 @@ static void wait_barrier(struct r10conf *conf)
 		 */
 		wait_event_lock_irq(conf->wait_barrier,
 				    !conf->barrier ||
-				    conf->nr_pending,
+				    (conf->nr_pending &&
+				     current->bio_list &&
+				     !bio_list_empty(current->bio_list)),
 				    conf->resync_lock);
 		conf->nr_waiting--;
 	}
@@ -1029,7 +1031,7 @@ static void raid10_unplug(struct blk_plug_cb *cb, bool from_schedule)
 	struct r10conf *conf = mddev->private;
 	struct bio *bio;
 
-	if (from_schedule) {
+	if (from_schedule || current->bio_list) {
 		spin_lock_irq(&conf->device_lock);
 		bio_list_merge(&conf->pending_bio_list, &plug->pending);
 		conf->pending_count += plug->pending_cnt;
